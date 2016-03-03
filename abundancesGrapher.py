236d4453eb26ec6f41cb897ca3b748a7f2613050
Adrian de Lomana <adrianlopezgarciadelomana@gmail.com>
2016-03-02 16:57:00 -0800

    adding a new file

---
+++ abundancesGrapher.py
@@ -0,0 +1,24 @@
+### this script selects the samples to be compared and plots heatmaps of different samples with the relevant abundances
+def metaDataReader():
+
+    '''
+    this function 
+    '''
+
+    return metaData
+### MAIN
+
+# 0. preliminaries
+
+# 0.1. metadata reader
+
+
+# 1. defining subsets of samples
+
+
+
+# 2. normalizing data
+
+# 3. trimming data
+
+# 4. plotting data

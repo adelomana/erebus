ed711f1120b828f5b71fb5933b6304a33028959e
Adrian de Lomana <adrianlopezgarciadelomana@gmail.com>
2016-03-10 15:52:10 -0800

    removing non using function

--- abundancesGrapher.py
+++ abundancesGrapher.py
@@ -53,23 +53,6 @@ def matrixDiscreteMaker(t):
 
     return t
 
-def discrete_cmap(N, base_cmap=None):
-    
-    '''
-    Create an N-bin discrete colormap from the specified input map
-    Modified from https://gist.github.com/jakevdp/91077b0cae40f8f8244a
-    '''
-
-    # Note that if base_cmap is a string or None, you can simply do
-    #    return plt.cm.get_cmap(base_cmap, N)
-    # The following works for string, None, or a colormap instance:
-
-    base = matplotlib.pyplot.cm.get_cmap(base_cmap)
-    color_list = base(numpy.linspace(0, 1, N))
-    cmap_name = base.name + str(N)
-    
-    return base.from_list(cmap_name, color_list, N)
-
 def metaDataReader():
 
     '''
